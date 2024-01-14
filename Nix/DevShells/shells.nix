c5347ff30b45548be2e564604a94ca34c018abe4
Pavel Lisitsyn <43790794+LisPavel@users.noreply.github.com>
2024-01-14 18:00:09 +0300

    feat: add node18 shell

--- Nix/DevShells/shells.nix
+++ Nix/DevShells/shells.nix
@@ -13,11 +13,25 @@
     '';
   };
 
+  node18 = pkgs.mkShell {
+#     name = "nodejs 18 version";
+
+    packages = with pkgs; [
+      nodejs_18
+      nodePackages.typescript
+    ];
+
+    shellHook = ''
+      echo "Entered node js 18 dev shell"
+    '';
+  };
+
   default = pkgs.mkShell {
 #     name = "nodejs 20 version";
 
     packages = with pkgs; [
       nodejs_20
+      nodePackages.typescript
     ];
 
     shellHook = ''
