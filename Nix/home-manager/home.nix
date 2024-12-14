e95e4ca425448f74512cbaeab313e5455a5ec913
Pavel Lisitsyn <43790794+LisPavel@users.noreply.github.com>
2024-12-14 12:54:29 +0300

    fix: yazi file manager

--- Nix/home-manager/home.nix
+++ Nix/home-manager/home.nix
@@ -202,6 +202,9 @@
   # --- yazi ---
   programs.yazi = {
     enable = true;
+    package = pkgs.yazi.override {
+      _7zz = (pkgs._7zz.override { useUasm = true; });
+    };
     enableFishIntegration = true;
   };
   # === yasi ===
