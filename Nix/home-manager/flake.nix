ae744c8fb002073f07327dc5c068b05f51495559
Pavel Lisitsyn <43790794+LisPavel@users.noreply.github.com>
2025-08-21 14:39:12 +0300

    cleanup flake

--- Nix/home-manager/flake.nix
+++ Nix/home-manager/flake.nix
@@ -4,15 +4,12 @@
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    # Specify nixpkgs deprecated (for nodejs 14 )
-    # nixpkgs-deprecated.url = "github:nixos/nixpkgs/nixos-23.05";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-#   outputs = { nixpkgs, home-manager, nixpkgs-deprecated, ... }:
   outputs = { nixpkgs, home-manager, ... }:
     let
       system = "x86_64-linux";
@@ -22,22 +19,9 @@
         # setup nixpkgs
         config = {
           allowUnfree = true;
-          permittedInsecurePackages = [
-            "electron-25.9.0"
-          ];
+          permittedInsecurePackages = [ "electron-25.9.0" ];
         };
       };
-
-      # set deprecated srcs
-  #       pkgs-dep = import nixpkgs-deprecated {
-  #         inherit system;
-  #         config = {
-  #           permittedInsecurePackages = [
-  #             "nodejs-14.21.3"
-  #             "openssl-1.1.1w"
-  #           ];
-  #         };
-  #       };
 
     in {
       home-manager.useUserPackages = true;
@@ -45,9 +29,9 @@
       homeConfigurations."pl" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
         # pass extra args to home manager
-#         extraSpecialArgs = {
-#           inherit pkgs-dep;
-#         };
+        #         extraSpecialArgs = {
+        #           inherit pkgs-dep;
+        #         };
 
         # Specify your home configuration modules here, for example,
         # the path to your home.nix.
